fe682ad30b34d399fc624e38032efcb4c04a87a8
irina-p-d <irinapetrovap@gmail.com>
2021-12-21 09:57:55 +0100

    first commit

---
+++ src/Controllers/RequestController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\View;
+use App\Models\RequestModel;
+
+class RequestController {
+
+    public function __construct()
+    {
+        
+        if(isset($_GET['action']) && $_GET['action'] == 'create')
+        {
+            $this->create();
+            return;
+        }
+
+        if(isset($_GET['action']) && $_GET['action'] == 'store')
+        {
+            $this->store($_POST);
+            return;
+        }
+
+        if(isset($_GET['action']) && $_GET['action'] == 'edit')
+        {
+            $id = $_GET['id_request'];
+            $this->edit($id);
+            return;
+        }
+
+        if(isset($_GET['action']) && $_GET['action'] == 'update')
+        {
+            $id = $_GET['id_request'];
+            $data = $_POST;
+            $this->update($id,$data);
+        }
+
+        if(isset($_GET['action']) && $_GET['action'] == 'delete')
+        {
+            $id = $_GET['id_request'];
+            $data = $_GET;
+            $this->delete($id,$data);
+            return;
+        }
+
+
+        $this->index();
+    }
+
+    public function index()
+    {
+        $data = RequestModel::all();
+        return new View('home', $data);
+    }
+
+    public function create()
+    {
+        return new View('create');
+    }
+
+    public function store($data) : void
+    {
+        $request = new RequestModel($data['topic'],$data['description'],$data['user_name']);
+        $request->save();
+        $this->index();
+    }
+
+    public function edit($id)
+    {
+        $data = RequestModel::findById($id);
+        return new View('edit', $data);
+    }
+
+    public function update($id,$data)
+    {
+        // Utilizando static method
+        RequestModel::update($id,$data);
+        $this->index();
+        // Utilizando public method
+        // $request = new RequestModel($data['topic'],$data['description'],$data['user_name'],$id);
+        // $request->update();
+    }
+
+    public function delete($id)
+    {   
+        RequestModel::delete($id);
+        $this->index();
+        
+    }
+
+}
